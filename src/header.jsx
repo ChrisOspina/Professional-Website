1ede4634fe2d45087bb317dd76a7f504eb9e15fc
Christopher Ospina <ChrisOspina99@gmail.com>
2024-07-29 11:40:14 -0400

    meta tags fix

--- src/header.jsx
+++ src/header.jsx
@@ -4,7 +4,6 @@ import './style.css';
 function Header(){
     return(
     <header className="siteHeader">
-         <div><meta name="viewport" content="width=device-width, initial-scale=1.0"></meta></div>
         <div className ="siteTitle">Christopher Ospina</div>
         <div className="siteDescription">Websites, Unix based modifications, Unity applications and Cybersecurity</div>
     </header>
