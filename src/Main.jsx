0fd59746af3e7acc1569394c5f2a17b0d931fe96
Christopher Ospina <ChrisOspina99@gmail.com>
2024-07-31 11:37:22 -0400

    Main content will now have multiple sections

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,32 +1,116 @@
 import React from "react";
 import './include/style.css';
 import profile from './profile.jpg';
+import nchalogo from './assets/National_HCA_Logo_RGB.png'
+import saslogo from './assets/saslogo.png'
 
 function Main(){
-    //TODO: update information
 return(
     <div className="mainContent">
         <div className="mainHeader">
             <h2 className="mainHeader-heading">About Me</h2>
         </div>
-        <div className="descripiton">
-            <p>I am currently an aspiring software engineer seeking oppurtunities to use the state-of-the-art technology to solve real-world problems. I work part time at SAS Technologies, 
-        a firm based in Hewlett, NY. Where I assist with the security and telecommunications systems of clients all over the U.S. such as Pace,
-        SVA and Touro.</p>
+
+        <section id="descripiton" className="section scrollspy">
+            <div className="containter flow-text">
+                <p>I am currently an aspiring software engineer seeking oppurtunities to use the state-of-the-art technology to solve real-world problems. 
+                I enjoy problem-solving and coding. Always strive to bring 100% to the work I do.
+
+                During my bachelor's and master's programs I have worked with technologies like C++, Python, Java, MySQL, The Unix Terminal, Node.js, and the Unity engine.
+                I have over four years of professional work expierence at companies like National Healthcare and The Simons Foundation which helped me
+                strenghthen my expertise of MySQL, PhP and Linux.
+
+                I am passionate about developing complex applications that solve real-world problems impacting millions of users. 
+                </p>
+
+                <ul>
+                    <li><b>Languages:</b> Python, Java, JavaScript, C, C++, C#, HTML/CSS, Sass, Php, Bash</li>
+                    <li><b>Databases:</b> MySQL, NoSQL</li>
+                    <li><b>Libraries:</b> NumPy, Pandas, OpenCV</li>
+                    <li><b>Frameworks:</b> React, Flask, DataTables, Node.js, Bootstrap, Apache</li>
+                    <li><b>Tools & Technologies:</b> Wodrpress, Git, Docker, AWS, WinSCP, Unity Engine, Netlify</li>
+                </ul>
+                <p>
+                Looking for an opportunity to work in a challenging position combining my skills in Software Engineering, which provides professional development, interesting experiences and personal growth.   
+                </p>
+            </div>
+        </section>
         <br/>
-        <p>My journey with information technology started during my junior year in high school where I took course in computer technology to fulfill
-        graduation requierements. From there I realized I wanted to pursue IT and software engineering as a career. At Farmingdale State College
-        I took programming courses that covered aspects such as Database Systems, Web Development, Systems Analysis & Design, UNIX Operating Systems,
-        Algorithms and Data Structures. During my tenure at Farmingdale, I partook in a summer research based project where I had the oppurtunity
-        to assist in building an ontology based search engine. I then presented my findings at the SURC Research Confrence at SUNY Old Westbury. 
-        After my time at Farmingdale, I continued my studies at the Seidenberg School of Computer Science and Information Systems at Pace University to obtain a masters degree.</p>
-        <br/>
-        <p>
-        Additionally, I have previously taken on a job at National Healthcare Associates Inc as a Junior Systems Adminstrator. From this job I have
-        learned both technical and transferrable skills which I am willing to present at any workplace environment. One of my major responsibilites
-        was to develop and mantain a tech website by designing clean and highly scalable code that compiles with best practices.
-        </p>
+
+        <section id="experience" className="section scrollspy">
+            <h2 className="mainHeader-heading">Experience</h2>
+            <div class="container">
+                <div class="card">
+                <div class="card-content">
+                    <div class="row">
+                    <div class="col s12 m2">
+                        <a href="https://www.nhca.com/"><img alt="National logo"
+                            src={nchalogo} class="responsive-img center-block" /></a>
+              </div>
+              <div class="col s12 m10">
+                <p>
+                  <span class="card-title"><a href="https://www.nhca.com/"
+                      class="teal-text hoverline">National Healthcare Associates, Inc.</a></span>
+                </p>
+              </div>
+            </div>
+            <div class="role brown-text text-darken-2">Junior Systems Administrator</div>
+            <ul>
+              <li>
+              Performed software maintenance & cleanup procedures to dispose of old stored data, ensuring hardware security & maintenance through configuring & encrypting laptops & Apple products for secure remote use.
+              </li>
+              <li>
+              Organized server room to comply with safety protocols & test device functionality 
+              </li>
+              <li>Automated employee report generation through web forms using PHP Form Builder, editing code with VS Code.</li>
+              <li>Developed web application to store & display employee, facility, procurement, & invoice data, automating data insertion from flat csv file into SQL database.</li>
+              <li>
+                <b>Tools:</b> MySQL, VS Code, PHP, Apache, JavaScript, Bootstrap, DataTables
+              </li>
+            </ul>
+          </div>
+          <div class="card-action">
+            <span>March 2020 - Jan 2023 | Valley Stream, NY</span>
+          </div>
         </div>
+        <br/>
+        <br/>
+        
+        <div class="card">
+          <div class="card-content">
+            <div class="row">
+              <div class="col s12 m2">
+                <a href="https://www.sastechnologies.com"><img alt="SAS logo"
+                    src={saslogo} class="responsive-img center-block" /></a>
+              </div>
+              <div class="col s12 m10">
+                <p>
+                  <span class="card-title"><a href="https://www.sastechnologies.com" target="_blank"
+                      class="teal-text hoverline">SAS Technologies</a></span>
+                </p>
+              </div>
+            </div>
+            <div class="role brown-text text-darken-2">Project Assistant</div>
+            <ul>
+              <li>
+              Configure card readers for school residence halls, assisting in equipment tally for camera & card reader installations.
+              </li>
+              <li>
+              Generate networking cable labels, programming security cameras by establishing circuit & configuring settings via app to ensure safety of institution & students.
+              </li>
+                <li><b>Tools:</b> DTools, Adobe Acrobat, Microsoft Office</li>
+            </ul>
+          </div>
+          <div class="card-action">
+            <span>Aug 2023 - Present | Hewlett, NY</span>
+          </div>
+        </div>
+
+
+
+            </div>
+        </section>
+
         <article className="profile">
             <figure className="profile_img_container">
                 <img className="profile_pic" src={profile} alt="profile"/>
