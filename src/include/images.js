4c8a96b9fd4a92ead0cc5ca7f4279d5b9576ddda
Christopher Ospina <ChrisOspina99@gmail.com>
2024-08-06 10:43:18 -0400

    added images for the skills section. For organization purposes images have folders by section

--- src/include/images.js
+++ src/include/images.js
@@ -1,9 +1,9 @@
 export { default as profile } from "../assets/images/profile.jpg";
-export { default as nhcalogo } from "../assets/images/National_HCA_Logo_RGB.png";
-export { default as saslogo } from "../assets/images/saslogo.png";
-export { default as simonslogo } from "../assets/images/01-SF-primary-logo-color.png";
-export { default as fsclogo} from "../assets/images/Farmingdale_State_College_logo_1.jpg";
-export { default as pacelogo} from "../assets/images/Pace_SEIDENBERG_2Color_RGB_Centered-01-2.jpg";
-export { default as Medxlogo} from '../assets/images/MedX_Logo.png';
-export { default as platePerflogo} from '../assets/images/platePerflogo.png'
+export { default as nhcalogo } from "../assets/images/exp/01-SF-primary-logo-color.png";
+export { default as saslogo } from "../assets/images/exp/saslogo.png";
+export { default as simonslogo } from "../assets/images/exp/01-SF-primary-logo-color.png";
+export { default as fsclogo} from "../assets/images/edu/Farmingdale_State_College_logo_1.jpg";
+export { default as pacelogo} from "../assets/images/edu/Pace_SEIDENBERG_2Color_RGB_Centered-01-2.jpg";
+export { default as Medxlogo} from '../assets/images/projects/MedX_Logo.png';
+export { default as platePerflogo} from '../assets/images/projects/platePerflogo.png';
 
