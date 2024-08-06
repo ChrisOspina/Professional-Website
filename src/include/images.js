2517a0b99ac17122e67e5ef1504aaac5c3db5652
Christopher Ospina <ChrisOspina99@gmail.com>
2024-08-06 12:52:58 -0400

    all images have been imported

--- src/include/images.js
+++ src/include/images.js
@@ -4,12 +4,40 @@ export { default as nhcalogo } from "../assets/images/exp/01-SF-primary-logo-col
 export { default as saslogo } from "../assets/images/exp/saslogo.png";
 export { default as simonslogo } from "../assets/images/exp/01-SF-primary-logo-color.png";
 //Education section
-export { default as fsclogo} from "../assets/images/edu/Farmingdale_State_College_logo_1.jpg";
-export { default as pacelogo} from "../assets/images/edu/Pace_SEIDENBERG_2Color_RGB_Centered-01-2.jpg";
+export { default as fsclogo } from "../assets/images/edu/Farmingdale_State_College_logo_1.jpg";
+export { default as pacelogo } from "../assets/images/edu/Pace_SEIDENBERG_2Color_RGB_Centered-01-2.jpg";
 //Projects section
-export { default as Medxlogo} from '../assets/images/projects/MedX_Logo.png';
-export { default as platePerflogo} from '../assets/images/projects/platePerflogo.png';
+export { default as Medxlogo } from '../assets/images/projects/MedX_Logo.png';
+export { default as platePerflogo } from '../assets/images/projects/platePerflogo.png';
 
 
 //Skills section
+
+//languages
+export { default as cslogo } from '../assets/images/skills/languages/c#logo.jpg';
+export { default as cpplogo } from '../assets/images/skills/languages/cpplogo.png';
+export { default as csslogo } from '../assets/images/skills/languages/css3.png';
+export { default as htmllogo } from '../assets/images/skills/languages/html5.png';
+export { default as javalogo } from '../assets/images/skills/languages/javalogo.png';
+export { default as javascriptlogo } from '../assets/images/skills/languages/js-logo-1.png';
+export { default as mysqllogo } from '../assets/images/skills/languages/mysql-logo-1-300x300.jpg';
+export { default as phplogo } from '../assets/images/skills/languages/php-logo-1.png';
+export { default as pythonlogo}  from  '../assets/images/skills/languages/python-logo-1.png';
+export { default as shelllogo} from '../assets/images/skills/languages/shell-logo-1.png';
+
+//libraries
+export { default as bootstrap } from '../assets/images/skills/libraries/bootstrap.png';
+export { default as numpylogo } from '../assets/images/skills/libraries/numpy-logo-1-500x500.jpg';
+export { default as pandaslogo } from '../assets/images/skills/libraries/pandas-logo-2.png';
+export { default as reactlogo} from '../assets/images/skills/libraries/react-original-wordmark-icon-840x1024-vhmauxp6.png';
+export { default as sckitlearn} from '../assets/images/skills/libraries/sk-learn-logo-1-500x500.jpg';
+
+//technologies
+export { default as apachelogo } from '../assets/images/skills/technologies/apache.jpeg';
+export { default as dockerlogo } from '../assets/images/skills/technologies/docker.png';
+export { default as gitlogo } from '../assets/images/skills/technologies/git.png';
+export { default as nodejslogo } from '../assets/images/skills/technologies/node.png';
+export { default as unitylogo} from '../assets/images/skills/technologies/Unity_Logo.png';
+
+/*Certifications will be added later*/
 
