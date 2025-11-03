3995b3565b187c9c8f7b9fb2538adbace163f8b1
ChrisOspina <ChrisOspina99@gmail.com>
2025-11-03 15:38:19 -0500

    Debugging form

--- src/Components/Contact-Form.jsx
+++ src/Components/Contact-Form.jsx
@@ -1,23 +1,21 @@
-import React from "react";
-
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
 import { Button } from "./ui/button";
 import { ContactSchema } from "../lib/contactSchema";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 const ContactForm = () => {
-  const form =
-    useForm <
-    z.infer <
-    typeof ContactSchema >>
-      {
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          username: "",
-        },
-      };
+  const form = useForm({
+    resolver: zodResolver(ContactSchema),
+    defaultValues: {
+      name: "",
+      email: "example@123.com",
+      message: "",
+    },
+  });
 
   return (
     <div className="flex flex-col items-center grid-cols-2">
@@ -28,18 +26,44 @@ const ContactForm = () => {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <Form {...form}>
+          {/* <Form {...form}>
             <form className="space-y-8">
               <FormField
                 control={form.control}
-                name="username"
+                name="name"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
                       <Input placeholder="your name" {...field} />
                     </FormControl>
-                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="example@123.com" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Message</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="your message goes here"
+                        {...field}
+                      />
+                    </FormControl>
                   </FormItem>
                 )}
               ></FormField>
@@ -48,7 +72,7 @@ const ContactForm = () => {
                 <Button type="submit">Submit</Button>
               </FormField>
             </form>
-          </Form>
+          </Form> */}
         </CardContent>
       </Card>
     </div>
